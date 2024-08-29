97762eb02f59154a7756bcaf1e203e817c123ee6
Michel <92521092+jacksonos@users.noreply.github.com>
2024-08-29 15:51:23 -0500

    create signup api with hashed Password with bdcryptjs

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+import User from '../model/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username.length === 0 ||
+    email.length === 0 ||
+    password.length === 0
+  ) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    res.json('signup successful');
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js'; //z
 
 dotenv.config();
 const app = express();
 
+app.use(express.json());
+
+//create mongodb connection
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -14,6 +19,11 @@ mongoose
     console.log(err);
   });
 
+//set server port to running
 app.listen(3000, () => {
   console.log('Server is running on port 3000 with nodemon 😈👌');
 });
+
+//api routes
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
