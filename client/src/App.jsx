a53a1cdf4ffa2e00c17281ae793f4f9544c091a4
Michel <92521092+jacksonos@users.noreply.github.com>
2024-08-28 23:25:41 -0500

    creation of header component and pages routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { SignIn } from './pages/SignIn';
+import { SignUp } from './pages/SignUp';
+import { Projects } from './pages/Projects';
+import { Dashboard } from './pages/Dashboard';
+import { Header } from './components/Header';
 
-export const App = () => {
+const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' elem ent={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/sign-in' element={<SignIn />}></Route>
+        <Route path='/sign-up' element={<SignUp />}></Route>
+        <Route path='/projects' element={<Projects />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
