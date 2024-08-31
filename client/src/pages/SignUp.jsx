cdc96a502cdee3c4066b138ad5efc466bc1daf90
Michel <92521092+jacksonos@users.noreply.github.com>
2024-08-31 00:18:30 -0500

    add  functions of error handle  in signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Label, TextInput } from 'flowbite-react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 
 export const SignUp = () => {
   const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill in all fields');
+    }
     try {
+      setLoading(true);
+      setErrorMessage(null);
       const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-    } catch (error) {}
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
     console.log(formData);
   };
 
@@ -73,8 +91,19 @@ export const SignUp = () => {
                 onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit'>
-              Submit
+            <Button
+              gradientDuoTone='purpleToPink'
+              type='submit'
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
             </Button>
           </form>
           <div className='flex flex-row gap-2 text-sm mt-5'>
@@ -83,6 +112,11 @@ export const SignUp = () => {
               Sign In
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
